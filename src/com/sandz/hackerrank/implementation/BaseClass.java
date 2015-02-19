b67b6968a09a164ce6b923533cfa58bf80bbe497
Sandeep <sandyridgeracer@gmail.com>
2015-02-19 14:15:48 +0530

    Making it non-static

--- src/com/sandz/hackerrank/implementation/BaseClass.java
+++ src/com/sandz/hackerrank/implementation/BaseClass.java
@@ -9,39 +9,48 @@ import java.io.PrintWriter;
 import java.util.StringTokenizer;
 
 public class BaseClass {
-    static BufferedReader br;
-    static StringTokenizer st;
-    static PrintWriter out;
+    public BufferedReader br;
+    public StringTokenizer st;
+    public PrintWriter out;
 
     public static void main(String[] args) throws IOException {
-        InputStream input = System.in;
-        PrintStream output = System.out;
-        br = new BufferedReader(new InputStreamReader(input));
-        out = new PrintWriter(output);
-        solve();
+        BaseClass solution = new BaseClass();
+        solution.initialize();
+        solution.solve();
+        solution.close();
+    }
+
+    private void close() throws IOException {
         br.close();
         out.close();
     }
 
-    private static void solve() throws IOException {
+    private void initialize() {
+        InputStream input = System.in;
+        br = new BufferedReader(new InputStreamReader(input));
+        PrintStream output = System.out;
+        out = new PrintWriter(output);
+    }
+
+    private void solve() throws IOException {
         nextDouble();
         nextInt();
         nextLong();
     }
 
-    private static long nextLong() throws IOException {
+    private long nextLong() throws IOException {
         return Long.parseLong(nextToken());
     }
 
-    private static double nextDouble() throws IOException {
+    private double nextDouble() throws IOException {
         return Double.parseDouble(nextToken());
     }
 
-    private static int nextInt() throws IOException {
+    private int nextInt() throws IOException {
         return Integer.parseInt(nextToken());
     }
 
-    private static String nextToken() throws IOException {
+    private String nextToken() throws IOException {
         while (st == null || !st.hasMoreTokens()) {
             String line = br.readLine();
             if (line == null) {
