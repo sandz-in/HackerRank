b914405a631fc24990b2682d6c1de8aa00b26404
Sandeep <sandyridgeracer@gmail.com>
2015-02-20 16:10:00 +0530

    Chocolate Feast

---
+++ src/com/sandz/hackerrank/warmup/ChocolateFeast.java
@@ -0,0 +1,75 @@
+package com.sandz.hackerrank.warmup;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class ChocolateFeast {
+    public BufferedReader br;
+    public StringTokenizer st;
+    public PrintWriter out;
+
+    private void solve() throws IOException {
+        int testCases = nextInt();
+        for (int i = 0; i < testCases; i++) {
+            int dollars = nextInt();
+            int price = nextInt();
+            int exchange = nextInt();
+            int foils = dollars / price;
+            int counter = foils;
+            while (foils >= exchange) {
+                int temp = foils;
+                int newWrappers = temp / exchange;
+                int leftOvers = temp % exchange;
+                foils = leftOvers + newWrappers;
+                counter += newWrappers;
+            }
+            printWithNewLine(counter);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ChocolateFeast solution = new ChocolateFeast();
+        solution.initialize();
+        solution.solve();
+        solution.close();
+    }
+
+    private void close() throws IOException {
+        br.close();
+        out.close();
+    }
+
+    private void initialize() {
+        InputStream input = System.in;
+        br = new BufferedReader(new InputStreamReader(input));
+        PrintStream output = System.out;
+        out = new PrintWriter(output);
+    }
+
+    private void printWithNewLine(Object o) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(o);
+        stringBuilder.append("\n");
+        out.write(stringBuilder.toString());
+    }
+
+    private int nextInt() throws IOException {
+        return Integer.parseInt(nextToken());
+    }
+
+    private String nextToken() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            String line = br.readLine();
+            if (line == null) {
+                return null;
+            }
+            st = new StringTokenizer(line);
+        }
+        return st.nextToken();
+    }
+}
