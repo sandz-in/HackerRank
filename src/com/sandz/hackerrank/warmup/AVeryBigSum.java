a3727c13d9a59af79f1b8528ef9f58daeada87c6
Sandeep Samdaria <sandyridgeracer@gmail.com>
2015-08-03 08:21:33 -0400

    Two new warmup problems.

---
+++ src/com/sandz/hackerrank/warmup/AVeryBigSum.java
@@ -0,0 +1,76 @@
+package com.sandz.hackerrank.warmup;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class AVeryBigSum {
+    public BufferedReader br;
+    public StringTokenizer st;
+    public PrintWriter out;
+
+    private void solve() throws IOException {
+        int n = nextInt();
+        long sum = 0;
+        while (n-- > 0) {
+            sum += nextInt();
+        }
+        println(sum);
+    }
+
+    public static void main(String[] args) throws IOException {
+        AVeryBigSum solution = new AVeryBigSum();
+        try {
+            solution.initialize();
+            solution.solve();
+        } finally {
+            solution.close();
+        }
+    }
+
+    private void close() throws IOException {
+        br.close();
+        out.close();
+    }
+
+    private void initialize() {
+        InputStream input = System.in;
+        br = new BufferedReader(new InputStreamReader(input));
+        PrintStream output = System.out;
+        out = new PrintWriter(output);
+    }
+
+    private void println(Object o) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(o);
+        stringBuilder.append("\n");
+        out.write(stringBuilder.toString());
+    }
+
+    private long nextLong() throws IOException {
+        return Long.parseLong(nextToken());
+    }
+
+    private double nextDouble() throws IOException {
+        return Double.parseDouble(nextToken());
+    }
+
+    private int nextInt() throws IOException {
+        return Integer.parseInt(nextToken());
+    }
+
+    private String nextToken() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            String line = br.readLine();
+            if (line == null) {
+                return null;
+            }
+            st = new StringTokenizer(line);
+        }
+        return st.nextToken();
+    }
+}
---
+++ src/com/sandz/hackerrank/warmup/DiagonalDifference.java
@@ -0,0 +1,78 @@
+package com.sandz.hackerrank.warmup;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class DiagonalDifference {
+    public BufferedReader br;
+    public StringTokenizer st;
+    public PrintWriter out;
+
+    private void solve() throws IOException {
+        int size = nextInt();
+        int matrix[][] = new int[size][size];
+        int lsum = 0;
+        int rsum = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrix[i][j] = nextInt();
+                if (i == j) {
+                    lsum += matrix[i][j];
+                }
+                if ((i + j) == (size - 1)) {
+                    rsum += matrix[i][j];
+                }
+            }
+        }
+        println(Math.abs(lsum - rsum));
+    }
+
+    public static void main(String[] args) throws IOException {
+        DiagonalDifference solution = new DiagonalDifference();
+        try {
+            solution.initialize();
+            solution.solve();
+        } finally {
+            solution.close();
+        }
+    }
+
+    private void close() throws IOException {
+        br.close();
+        out.close();
+    }
+
+    private void initialize() {
+        InputStream input = System.in;
+        br = new BufferedReader(new InputStreamReader(input));
+        PrintStream output = System.out;
+        out = new PrintWriter(output);
+    }
+
+    private void println(Object o) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(o);
+        stringBuilder.append("\n");
+        out.write(stringBuilder.toString());
+    }
+
+    private int nextInt() throws IOException {
+        return Integer.parseInt(nextToken());
+    }
+
+    private String nextToken() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            String line = br.readLine();
+            if (line == null) {
+                return null;
+            }
+            st = new StringTokenizer(line);
+        }
+        return st.nextToken();
+    }
+}
